ff72479a25ceea8c5abe41c8051057b5f243dad5
Ink22 <95543290+Ink22@users.noreply.github.com>
2025-05-17 12:52:42 -0700

    Text changes

--- src/App.svelte
+++ src/App.svelte
@@ -21,6 +21,7 @@
         class="w-screen h-5 pb-1 inline-flex flex-col justify-start items-right gap-5 bg-zinc-950"
         style="app-region: drag;"
     ></section>
+
     <section
         class="w-screen pt-3 pb-3 inline-flex flex-col justify-start items-center gap-5"
     >
@@ -34,14 +35,16 @@
 				
             />
             <div
-                class="mt-4 w-24 h-12 justify-start hover:drop-shadow-[0_0_0.8rem_#e85f24] transition-all duration-900 text-white text-3xl font-extralight"
+                class="mt-3 w-577 h-103 justify-start hover:drop-shadow-[0_0_0.8rem_#e85f24] transition-all duration-900 text-white text-3xl font-(family-name:IBM Plex Sans KR)"
             >
-                Home
+                N.11 PIZZA
             </div>
         </div>
         <div class="self-stretch outline-[2px] outline-gray-900"></div>
     </section>
 
+    
+    
     <section class="flex flex-col items-center justify-center mt-1">
         <div class="text-3xl font-(family-name:IBM Plex Sans KR)">{TritonMessage[switchUIInt]}</div>
         <div class="text-2xl font-(family-name:IBM Plex Sans KR)">Version 0.0.1</div>
