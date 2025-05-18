9ec08ac41650ab22fed8add189b8af15eeb12d66
Ink22 <95543290+Ink22@users.noreply.github.com>
2025-05-17 17:09:29 -0700

    First Section Draft #1

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,13 @@
 <script>
   import Logo from "./assets/n11logo.svg";
+  import Map from "./lib/Map.svelte";
   const nodeVersion = api.node();
   const chromeVersion = api.chrome();
   let switchUI = false;
   let switchUIInt = 0;
   let TritonMessage = ["Triton", "altTriton"];
-  let firstSection = ["1. Prompt First Responders", "1. Select Store"];
+
+  let firstSection = ["1. PROMPT FIRST RESPONDERS", "1. SELECT STORE"];
   let firstQuestion = [
     "What type of assistance do you need? (Select Multiple)",
     "Would you like to filter out stores without seasonal options?",
@@ -13,6 +15,13 @@
   let policeBtn = ["Police", "Yessir"];
   let medicalBtn = ["Medical", "No Sir :("];
   let secondQuestion = ["Where are you right now?", "Where are you right now?"];
+  let thirdQuestion = ["Are you being moved right now?", "Delivery Method"];
+  let beingMovedYes = ["YES", "PICKUP"];
+  let beingMovedNo = ["NO", "DELIVERY"];
+  let beingMovedBox = ["Describe your surroundings or Vehicle", "Describe your vehicle or house"];
+  let streamStartBtn = ["BEGIN STREAMING WITH OPERATOR", "START ORDER"];
+
+  let secondSection = ["2. DESCRIBE THE SITUATION", "2. CUSTOMIZE YOUR CHEESE"];
 
   function altUI() {
     switchUI = !switchUI;
@@ -76,10 +85,10 @@
           {firstSection[switchUIInt]}
         </div>
       </div>
-      <!-- Cream Section -->
+      <!-- Cream Section #1  -->
       <div class="w-full flex flex-col items-center size-100 p-4 bg-offwhite">
         <!-- What type of assistance do you need? -->
-        <div class="text-2xl font-IBM text-black">
+        <div class="text-2xl font-IBM text-black font-bold">
           {firstQuestion[switchUIInt]}
         </div>
         <!--Police Button-->
@@ -87,15 +96,15 @@
           <div
             class="w-fit mx-2 bg-greyblue flex flex-box float-left rounded-lg"
           >
-            <div class="m-5 text-3xl font-IBM font-light text-white">
+            <button class="m-5 text-xl font-IBM font-light text-white">
               {policeBtn[switchUIInt]}
-            </div>
+            </button>
           </div>
           <!--Medical Button-->
           <div
             class="w-fit mx-2 bg-greyblue flex flex-box float-left rounded-lg"
           >
-            <button class="m-5 text-3xl font-IBM font-light text-white">
+            <button class="m-5 text-xl font-IBM font-light text-white">
               {medicalBtn[switchUIInt]}
             </button>
           </div>
@@ -104,25 +113,75 @@
         <div class="mt-4 self-stretch outline-[2px] outline-black"></div>
 
         <!-- Where are you right now? -->
-        <div class="text-2xl font-IBM text-black">
+        <div class="text-2xl font-IBM text-black font-bold">
           {secondQuestion[switchUIInt]}
         </div>
 
         <!--Where are you right now? text box-->
-        <div class="items-center justify-center w-fit self-stretch box-border size-32 border-4 p-4">
-          <div class="relative mb-4">
-            <label for="email" class="leading-7 text-sm text-gray-600"
-              >Email</label
-            >
+          <div class="">
             <input
               type="email"
               id="email"
               name="email"
-              class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              placeholder="ADDRESS BOX"
+              class="w-fit bg-white rounded border-4 border-red-dark border- focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
             />
+        </div>
+        <div class= "mt-4 text-2xl font-IBM text-black font-bold">
+          OR
+        </div>
+        <!-- Google Maps Display-->
+        <div class="w-screen"><Map /></div>
+
+        <!--Line Break-->
+        <div class="mt-4 self-stretch outline-[2px] outline-black"></div>
+
+        <!-- Are you being moved right now? -->
+        <div class="mt-4 text-2xl font-IBM text-black font-bold">
+          {thirdQuestion[switchUIInt]}
+        </div>
+        <div class="mt-4 flex">
+          <div
+            class="w-fit mx-2 bg-greyblue flex flex-box float-left rounded-lg"
+          >
+          <!-- Yes being Moved Button-->
+            <button class="m-5 text-xl font-IBM font-normal text-white">
+              {beingMovedYes[switchUIInt]}
+            </button>
+          </div>
+          <!--NO being Moved Button-->
+          <div
+            class="w-fit mx-2 bg-greyblue flex flex-box float-left rounded-lg"
+          >
+            <button class="m-5 text-xl font-IBM font-normal text-white">
+              {beingMovedNo[switchUIInt]}
+            </button>
           </div>
         </div>
+
+        <!-- Describe your surroundings text box-->
+          <div class="mt-4 mb-4">
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder= {beingMovedBox[switchUIInt]}
+              class="w-fit bg-white rounded border-4 border-red-dark border- focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+        </div>
+
+        <!-- Line Break -->
+        <div class="mt-4 self-stretch outline-[2px] outline-black"></div>
+        
+        <!-- Stream Button -->
+        <div class="w-fit mt-4 bg-greyblue flex flex-box float-left rounded-lg">
+          <button class="m-5 text-xl font-IBM text-white font-normal">
+            {streamStartBtn[switchUIInt]}
+          </button>
+        </div>
+
       </div>
     </div>
   </section>
+  <!-- End Section #1 -->
 </main>
